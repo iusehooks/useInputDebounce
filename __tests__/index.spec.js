8fbff0ec1e2d3055635a18084fd1513225a5438a
Brian Andrews <sbardian@gmail.com>
2019-02-13 12:17:19 -0700

    Add tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,7 +1,59 @@
-// import useInputDebounce from "./../src";
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import useInputDebounce from "./../src";
+import { debug } from "util";
+
+const effect = jest.fn();
+
+const DebouncedComponent = ({ delay, minLength }) => {
+  const attributes = useInputDebounce(effect, { delay, minLength });
+  return <input data-testid="my-test-input" {...attributes} />;
+};
 
 describe("useInputDebounce", () => {
-  it("need some test", () => {
-    expect(true).toBe(true);
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    cleanup();
+    effect.mockReset();
+  });
+
+  it("should call effect once for value change", () => {
+    const { getByTestId } = render(
+      <DebouncedComponent delay={1000} minLength={1} />
+    );
+    const dInput = getByTestId("my-test-input");
+    fireEvent.change(dInput, { target: { value: "a" } });
+    jest.runAllTimers();
+    expect(effect).toHaveBeenCalledTimes(1);
+  });
+  it("shouldn't call effect until minLength has been reached", () => {
+    const { getByTestId } = render(
+      <DebouncedComponent delay={1000} minLength={2} />
+    );
+    const dInput = getByTestId("my-test-input");
+    fireEvent.change(dInput, { target: { value: "a" } });
+    jest.runAllTimers();
+    expect(effect).not.toHaveBeenCalled();
+    fireEvent.change(dInput, { target: { value: "ab" } });
+    jest.runAllTimers();
+    expect(effect).toBeCalledTimes(1);
+  });
+  it("called with no delay or minLength", () => {
+    const { getByTestId } = render(<DebouncedComponent />);
+    const dInput = getByTestId("my-test-input");
+    fireEvent.change(dInput, { target: { value: "ab" } });
+    jest.runAllTimers();
+    expect(effect).toBeCalledTimes(1);
+  });
+  it("setTimeout", () => {
+    const { getByTestId, debug } = render(
+      <DebouncedComponent delay={1000} minLength={1} />
+    );
+    const dInput = getByTestId("my-test-input");
+    fireEvent.change(dInput, { target: { value: "a" } });
+    jest.advanceTimersByTime(1500);
+    expect(effect).toBeCalledTimes(1);
   });
 });
